59df7290d04943b752c921fbb81e6c1de2444852
Andres Narvaez <adnarvaezp@hotmail.com>
2025-09-18 22:47:47 -0500

    simplifique el code

--- 5.py
+++ 5.py
@@ -1,11 +1,9 @@
 # programa numeros del 0 al 10
 for i in range(11):
     print (i)
-print("hola mundo")
-print("hola mundo")
+
 print("hola mundo")
 list1=[22,33,44,5,66,77,88]
-
+ 
 print("hola mundo")
-
 print("hola mundo")
--- 55.py
+++ 55.py
@@ -1,10 +1,6 @@
 print("hola mundo")
-print("hola mundo")
-print("hola mundo")
-print("hola mundo")
 list1=[22,33,44,5,66,77,88]
 for i in  list1:
     print(i)
-
 print("hola mundo")
 print("hola mundo")
