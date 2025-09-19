1347db8ccdcf3757b6d913acaf7c69f021622de1
Andres Narvaez <adnarvaezp@hotmail.com>
2025-09-18 22:39:13 -0500

    segundo cambio

--- 5.py
+++ 5.py
@@ -3,3 +3,9 @@ for i in range(11):
     print (i)
 print("hola mundo")
 print("hola mundo")
+print("hola mundo")
+list1=[22,33,44,5,66,77,88]
+
+print("hola mundo")
+
+print("hola mundo")
--- 55.py
+++ 55.py
@@ -3,5 +3,8 @@ print("hola mundo")
 print("hola mundo")
 print("hola mundo")
 list1=[22,33,44,5,66,77,88]
-for i in list1:
+for i in  list1:
     print(i)
+
+print("hola mundo")
+print("hola mundo")
