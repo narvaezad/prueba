3102d9837b4ff59608678ebea6bf61c44f8b1a98
Andres Narvaez <adnarvaezp@hotmail.com>
2025-09-18 22:59:57 -0500

    3 cambio

--- 5.py
+++ 5.py
@@ -1,9 +1,3 @@
 # programa numeros del 0 al 10
 for i in range(11):
     print (i)
-
-print("hola mundo")
-list1=[22,33,44,5,66,77,88]
- 
-print("hola mundo")
-print("hola mundo")
--- 55.py
+++ 55.py
@@ -1,6 +1,3 @@
-print("hola mundo")
 list1=[22,33,44,5,66,77,88]
 for i in  list1:
     print(i)
-print("hola mundo")
-print("hola mundo")
